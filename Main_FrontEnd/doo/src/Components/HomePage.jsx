bd02ce659ed738f8ee371a24d1c9c826fab3abd8
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-12-12 11:23:07 +0530

    Update HomePage.jsx

--- Main_FrontEnd/doo/src/Components/HomePage.jsx
+++ Main_FrontEnd/doo/src/Components/HomePage.jsx
@@ -5,9 +5,9 @@ export const HomePage = () =>
 { const navigate = useNavigate()
   return (
     <div>
-          <h1>Home Page</h1>
-        <button onClick={()=> navigate("/login")}>Login</button>
-        <button onClick={()=>navigate("/signup")}>SignUp</button>
+            <h1>Home Page</h1>
+          <button onClick={()=> navigate("/login")}>Login</button>
+          <button onClick={()=>navigate("/signup")}>SignUp</button>
     </div>
   )
 }
